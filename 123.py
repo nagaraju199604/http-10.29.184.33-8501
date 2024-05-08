a9c6ee6ae05af2cd5a85d199c705e3fe823f0611
nagaraju199604 <169065433+nagaraju199604@users.noreply.github.com>
2024-05-08 15:31:03 +0530

    Update 123.py

--- 123.py
+++ 123.py
@@ -4,8 +4,7 @@
 # In[1]:
 
 
-import asyncio
-asyncio.set_event_loop(asyncio.new_event_loop())
+
 
 
 # In[2]:
@@ -13,20 +12,14 @@ asyncio.set_event_loop(asyncio.new_event_loop())
 
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain_community.chat_models import ChatOpenAI
 
-from streamlit_jupyter import StreamlitPatcher
 
 OPENAI_API_KEY ="sk-proj-aHhz1tv0D9lU1EZpWMxlT3BlbkFJfi8i4BQnrE1GXo3NK0oI"
 
 # In[3]:
 
 
-StreamlitPatcher().jupyter() 
+
 
 
 # In[4]:
@@ -45,46 +38,11 @@ if file is not None:
     text = ""
     for page in pdf_reader.pages:
         text += page.extract_text()
-        #st.write(text)
-
-#Break it into chunks
-    text_splitter = RecursiveCharacterTextSplitter(
-        separators="\n",
-        chunk_size=1000,
-        chunk_overlap=150,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    #st.write(chunks)
+        st.write(text)
 
 
-    # generating embedding
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
-    # creating vector store - FAISS
-    vector_store = FAISS.from_texts(chunks, embeddings)
-
-    # get user question
-    user_question = st.text_input("Type Your question here")
-
-    # do similarity search
-    if user_question:
-        match = vector_store.similarity_search(user_question)
-        #st.write(match)
-
-        #define the LLM
-        llm = ChatOpenAI(
-            openai_api_key = OPENAI_API_KEY,
-            temperature = 0,
-            max_tokens = 1000,
-            model_name = "gpt-3.5-turbo"
-        )
-
-        #output results
-        #chain -> take the question, get relevant document, pass it to the LLM, generate the output
-        chain = load_qa_chain(llm, chain_type="stuff")
-        response = chain.run(input_documents = match, question = user_question)
-        st.write(response)
-
-
+   
+    
+        
 
