e53867616950d30c3b5bf01693612fd2182a0096
nagaraju199604 <169065433+nagaraju199604@users.noreply.github.com>
2024-05-08 16:28:57 +0530

    Update 123.py

--- 123.py
+++ 123.py
@@ -31,7 +31,7 @@ if file is not None:
     )
     chunks=text_splitter.split_text(text)
     #print(st.write(chunks))
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-    vector_store = FAISS.from_texts(chunks, embeddings)
+    #embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    #vector_store = FAISS.from_texts(chunks, embeddings)
     #user_question = st.text_input("Type Your question here")
 
